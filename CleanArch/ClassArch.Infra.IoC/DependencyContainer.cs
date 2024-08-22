04f8a5b75bdfef6c2e0c310e7f503fc8f2cd5163
Sowjanya <schandrala-c@illuminateed.net>
2024-08-22 17:46:18 +0530

    CommandHandlers

--- CleanArch/ClassArch.Infra.IoC/DependencyContainer.cs
+++ CleanArch/ClassArch.Infra.IoC/DependencyContainer.cs
@@ -8,6 +8,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CleanArch.Domain.Core.Bus;
+using Microsoft.IdentityModel.Tokens;
+using CleanArch.Infra.Bus;
+using MediatR;
+using CleanArch.Domain.Commands;
+using CleanArch.Domain.CommandHandlers;
+using CleanArch.Infra.Data.Context;
 
 namespace ClassArch.Infra.IoC
 {
@@ -15,11 +22,18 @@ namespace ClassArch.Infra.IoC
     {
         public static void RegisterService( IServiceCollection services)
         {
+            //Domain In memory bus
+            services.AddScoped<IMediatorHandler, InMemoryBus>();
+
+            //Domain Handler
+            services.AddScoped<IRequestHandler<CreateCourseCommand, bool>, CourseCommandHandler>();
+
             //Application Layer
             services.AddScoped<ICourseService, CourseService>();
 
             //Data.Infra Layer
             services.AddScoped<ICourseRepository, CourseRepository>();
+            services.AddScoped<UniversityDBContext>();
         }
     }
 }
